ea95fd460656f8ea7716123db40716b0e6cc3b32
DuyBach <duybach@live.de>
2015-05-13 16:08:17 -0700

    TEST

--- Systemprogrammierung/main.c
+++ Systemprogrammierung/main.c
@@ -131,15 +131,102 @@ int raytracer_parallel(const char* filename, int processcount){
 	calculate_casting_bounds(scene->cam, bounds);
 
 	//TODO: spawn processes for each frame
+	int space = HEIGHT / processcount;
+	int splitArray[processcount][2];
+
+	splitArray[0][0] = 0;
+	splitArray[0][1] = space;
+
+	int i;
+	for(i = 1; i < processcount; i++) {
+		splitArray[i][0] = splitArray[i-1][0] + space;
+		splitArray[i][1] = splitArray[i-1][1] + space;
+	}
+
+	FILE *file = fopen(filename, "wb");
+	write_bitmap_header(file, WIDTH, HEIGHT);
+	fclose(file);
+
+	pid_t pid;
+	int counter = 0;
+
+
+	for(i = 0; i < processcount-1; i++) {
+		pid = fork();
+		
+		if(pid == 0) {
+			//if child
+			//break out of for-loop
+			break;
+		} else if ( pid > 0) {
+			//if parent
+			//counter++ and keep fork() up;
+			counter++;
+		} else {
+			//fork failed
+			printf("fork() failed!\n");
+			return EXIT_FAILURE;
+		}
+	}
 
 	//TODO: seek to the right position in the bitmap file and write frame
+	// Allocate buffer for picture data
+	pix_t *img = (pix_t*) calloc(HEIGHT * WIDTH, sizeof(pix_t));
+	if (img){
+
+		// calculate the data for the image (do the actual raytrace)
+		raytrace(img, bounds, scene, 0, splitArray[counter][0], WIDTH, splitArray[counter][1]);
+
+		delete_scene(scene);
+
+		// open file
+		FILE *file = fopen(filename, "ab");
+		if (file) {
+			/*
+			int size = 26;
+			
+			if (processcount > 1) {
+				while((counter*(1440000/(processcount)))+26 != size) {
+					fseek(file, 0, SEEK_END); // seek to end of file
+					size = ftell(file); // get current file pointer
+					fseek(file, 0, SEEK_SET); // seek back to beginning of file
+				}
+			}*/
+
+			// write image to file on disk
+			fwrite(img, 3, WIDTH * space, file);
+
+			/*
+			fseek(file, 0, SEEK_END); // seek to end of file
+			size = ftell(file); // get current file pointer
+			fseek(file, 0, SEEK_SET); // seek back to beginning of
+
+			printf("%d\n", size);
+			*/
+
+			// free buffer
+			free(img);
+		
+			// close file
+			fclose(file);
+		}
+	}
 
 	//TODO: wait for all child processes to finish before stopping time
+	if (counter != processcount-1) {
+		exit(0);
+	}
+
 
 	// print the measured time
+	int status = 0;
+	pid_t wpid;
+	while ((wpid = wait(&status)) > 0);
+
 	end = current_time_millis();
 	printf("Render time: %.3fs\n", (double) (end - start) / 1000);
 	
+
 	return EXIT_SUCCESS;
 	//return EXIT_FAILURE;
 }
