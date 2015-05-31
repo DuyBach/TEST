d3167fc8821438bd2d165cfc016efc89e5fbd352
DuyBach <duybach@live.de>
2015-05-31 15:19:15 -0700

    done

--- Systemprogrammierung/MLF.c
+++ Systemprogrammierung/MLF.c
@@ -9,8 +9,8 @@
 typedef struct q_elem{
 	int id;
 	int length;
-	int time_step;
 	int time_left;
+	int nr;
 	struct q_elem *next;
 } q_elem;
 
@@ -22,14 +22,16 @@ struct Queue{
 
 struct QueueList{
 	struct Queue *root;
+	q_elem *actual;
 	int num_queues;
+	int time_step;
 };
 
 
 struct QueueList *queList;
 
 
-void add_que(int tmp_step)
+void add_que()
 {
 	struct Queue *que = (struct Queue*)malloc(sizeof(struct Queue));
 	if (que == NULL) {
@@ -38,7 +40,6 @@ void add_que(int tmp_step)
 	}
 
 	que->root = NULL;
-	que->time_step = tmp_step;
 
 	if (queList->root == NULL) {
 		queList->root = que;
@@ -46,6 +47,8 @@ void add_que(int tmp_step)
 		que->next = queList->root;
 		queList->root = que;
 	}
+
+	switch_task(IDLE);
 }
 
 //FIFO when q_elem->time_step == num_queues*time_step
@@ -58,15 +61,14 @@ int init_MLF(int time_step, int num_queues)
 		exit(1);
 	}
 
+	queList->actual = NULL;
 	queList->num_queues = num_queues;
+	queList->time_step = time_step;
 
-	int tmp_step = num_queues*time_step;
 	
 	int i;
 	for (i = 0; i < num_queues; i++) {
-		add_que(tmp_step);
-		//recalculate tmp_step
-		tmp_step -= time_step;
+		add_que();
 	}
 	
 	return 1;
@@ -75,19 +77,152 @@ int init_MLF(int time_step, int num_queues)
 void free_MLF()
 {
 	// TODO
+	// zuerst queue elemete freen
+	// danach queue freen
+	// danach queuelist freen
+
+	q_elem *tmpEle, *toDelete;
+	struct Queue *queDelete;
+	struct Queue *current = queList->root;
+
+	int i;
+	for (i = 0; i < queList->num_queues; i++) {
+		// delete queue elements		
+		tmpEle = current->root;
+		while(tmpEle != NULL) {
+			toDelete = tmpEle;
+			tmpEle = tmpEle->next;
+
+			free(toDelete);
+		}
+
+		queDelete = current;
+		current = current->next;
+
+		free(queDelete);		
+	}
+
+	free(queList);
 }
 
 void arrive_MLF(int id, int length)
 {
-	id = id;
-	length = length;
 	// TODO
+	q_elem *new = (q_elem*)malloc(sizeof(q_elem));
+	new->id = id;
+	new->length = length;
+	new->nr = 0;
+	new->next = NULL;
+	new->time_left = queList->time_step;
+
+	if (queList->actual == NULL) {
+		queList->actual = new;
+
+		switch_task(new->id);
+
+		return;
+	}
 	
+	if (queList->root->root == NULL) {
+		queList->root->root = new;
+	} else {
+		q_elem *current;
+		current = queList->root->root;	
+		
+		while (1) {
+			if (current->next == NULL) {
+				current->next = new;
+				return;
+			}
+
+			current = current->next;
+		}
+	}
 }
 
 void tick_MLF()
 {
 	// TODO
+	if(queList->actual == NULL) {
+		switch_task(IDLE);
+
+		return;
+	}
+
+	queList->actual->time_left--;
+	queList->actual->length--;
+
+	if (queList->actual->length == 0) {
+		free(queList->actual);
+
+		//search for best new work
+		struct Queue *current = queList->root;
+
+		int i;
+		for (i = 0; i < queList->num_queues; i++) {
+			if (current->root != NULL) {
+				queList->actual = current->root;
+				current->root = current->root->next;				
+				
+				switch_task(queList->actual->id);
+
+				return;
+			}
+
+			current = current->next;
+		}
+
+		switch_task(IDLE);
+	} else if (queList->actual->time_left == 0) {
+		//put in next que
+		queList->actual->nr++;
+
+		if (queList->actual->nr == queList->num_queues) {
+			queList->actual->time_left = 255;
+		} else {
+			queList->actual->time_left = queList->actual->nr * queList->time_step;
+		}
+		struct Queue *current = queList->root;
+
+		int i;
+		for (i = 0; i < queList->actual->nr; i++) {
+			current = current->next;
+		}
+
+		q_elem *mover = current->root;
+
+		if(mover == NULL) {
+			current->root = queList->actual;
+		} else {		
+			while (1) {
+				if (mover->next == NULL) {
+					mover->next = queList->actual;
+
+					break;
+				}
+
+				mover = mover->next;
+			}
+		}
+		
+		//search for best new work
+		current = queList->root;
+
+		for (i = 0; i < queList->num_queues; i++) {
+			if (current->root != NULL) {
+				queList->actual = current->root;
+				current->root = current->root->next;				
+				
+				switch_task(queList->actual->id);
+
+				return;
+			}
+
+			current = current->next;
+		}
+
+		switch_task(IDLE);
+	}
 }
 
 void finish_MLF()
--- Systemprogrammierung/main.c
+++ Systemprogrammierung/main.c
@@ -1,57 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "SRTN.h"
+#include "MLF.h"
 #include "task.h"
 
 
-//for SJN & SRTN
-/*
+//for MLF & MLF
 int main()
 {
-	// just a very basic example to show you, how to test your implementation (in this case: SRTN)
-	if (init_SRTN())
+	if (init_MLF(4, 4))
 	{
-		arrive_SRTN(0, 1);
-		arrive_SRTN(1, 2);
-		arrive_SRTN(2, 3);
-
-		printf("%i\n", running_task);	// should print: 0
-		finish_SRTN();
-		printf("%i\n", running_task);	// should print: 1
-		finish_SRTN();
-		printf("%i\n", running_task);	// should print: 2
-		finish_SRTN();
-		printf("%i\n", running_task);	// should print: -1 (no process left)
+		printf("%i\n", running_task); // -1
+		arrive_MLF(10, 5);
+		printf("%i\n", running_task); // 10
+		tick_MLF();
+		tick_MLF();
+		arrive_MLF(2, 10);
+		tick_MLF();
+		tick_MLF();
+		tick_MLF();
+		printf("%i\n", running_task); // 2
 	}
 	else
 	{
-		printf("SRTN is not implemented yet. Quitting.\n");
-	}
-
-	return 0;
-}
-*/
-
-//for SRTN & MLF
-int main()
-{
-	if (init_SRTN())
-	{
-		printf("%i\n", running_task); // should print: -1
-		arrive_SRTN(20, 5);
-		arrive_SRTN(10, 5);
-		printf("%i\n", running_task); // should print: 20
-		tick_SRTN();
-		tick_SRTN();
-		tick_SRTN();
-		tick_SRTN();
-		tick_SRTN();
-		printf("%i\n", running_task); // should print: 10
-	}
-	else
-	{
-		printf("SRTN is not implemented yet. Quitting.\n");
+		printf("MLF is not implemented yet. Quitting.\n");
 	}
 
 	return 0;
